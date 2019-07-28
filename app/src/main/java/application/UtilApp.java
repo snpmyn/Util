a80f851b9e152144cbd2128c3121c902b55606ef
snpmyn <snpmyn@126.com>
2019-07-28 20:27:35 +0800

    优化

--- app/src/main/java/application/UtilApp.java
+++ app/src/main/java/application/UtilApp.java
@@ -1,13 +1,20 @@
 package application;
-
+import com.squareup.leakcanary.LeakCanary;
 import com.zsp.application.UtilApplication;
-
 /**
  * Created on 2019/7/28.
- *
  * @author 郑少鹏
  * @desc 应用
  */
 public class UtilApp extends UtilApplication {
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+    }
 }
