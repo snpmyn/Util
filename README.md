8fb14bb38f7743555622aa675d9c930fc8d64940
snpmyn <snpmyn@126.com>
2019-07-29 09:53:28 +0800

    优化

--- README.md
+++ README.md
@@ -8,3 +8,8 @@
 * api 'com.jakewharton.timber:timber:4.7.1'（避重）
 * implementation 'com.qw:soulpermission:1.1.8'
 * implementation 'org.apache.commons:commons-lang3:3.9'
+
+### 权限
+#### app
+* <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />（避重）
+* <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE" />（避重）
--- app/src/main/java/com/zsp/util/MainActivity.java
+++ app/src/main/java/com/zsp/util/MainActivity.java
@@ -1,11 +1,14 @@
 package com.zsp.util;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.zsp.utilone.intent.IntentUtils;
+import com.zsp.utilone.permission.SoulPermissionUtils;
+import com.zsp.utilone.toast.ToastUtils;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -17,11 +20,39 @@ import example.TimberActivity;
  * @date: 2019/6/2 17:44
  */
 public class MainActivity extends AppCompatActivity {
+    private SoulPermissionUtils soulPermissionUtils;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initConfiguration();
+        execute();
+    }
+
+    private void initConfiguration() {
+        soulPermissionUtils = new SoulPermissionUtils();
+    }
+
+    private void execute() {
+        soulPermissionUtils.checkAndRequestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, soulPermissionUtils,
+                true, new SoulPermissionUtils.CheckAndRequestPermissionCallBack() {
+                    @Override
+                    public void onPermissionOk() {
+
+                    }
+
+                    @Override
+                    public void onPermissionDeniedNotRationaleInMiUi(String s) {
+                        ToastUtils.shortShow(MainActivity.this, s);
+                    }
+
+                    @Override
+                    public void onPermissionDeniedNotRationaleWithoutLoopHint(String s) {
+
+                    }
+                });
     }
 
     @OnClick(R.id.mainActivityMbTimber)
