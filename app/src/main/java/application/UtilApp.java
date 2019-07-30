c28455519991c3d789c0cb0a96c5e562fbd5fa7c
snpmyn <snpmyn@126.com>
2019-07-30 16:15:21 +0800

    内存泄漏更新

--- app/src/main/java/application/UtilApp.java
+++ app/src/main/java/application/UtilApp.java
@@ -2,7 +2,6 @@ package application;
 
 import android.app.Application;
 
-import com.squareup.leakcanary.LeakCanary;
 import com.zsp.util.BuildConfig;
 import com.zsp.utilone.timber.configure.TimberInitConfigure;
 
@@ -16,12 +15,6 @@ public class UtilApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
         TimberInitConfigure.initTimber(BuildConfig.DEBUG);
     }
 }
