cbf726bdfee1759e38252b4d8ab3f98a27ee46ed
snpmyn <snpmyn@126.com>
2019-08-20 12:05:06 +0800

    优化

--- utiltwo/src/main/java/com/zsp/utiltwo/draw/DrawUtils.kt
+++ utiltwo/src/main/java/com/zsp/utiltwo/draw/DrawUtils.kt
@@ -1,11 +1,6 @@
-@file:JvmName("DrawUtils")
-
 package com.zsp.utiltwo.draw
 
-import android.graphics.Color
 import android.graphics.Paint
-import android.view.View
-import androidx.annotation.ColorInt
 
 /**
  * Created on 2019/8/19.
@@ -13,29 +8,6 @@ import androidx.annotation.ColorInt
  * @desc DrawUtils
  */
 object DrawUtils {
-    /**
-     * 创画笔
-     */
-    @JvmOverloads
-    fun <T : View> T.createPaint(colorString: String? = null, @ColorInt color: Int? = null): Paint {
-        return Paint().apply {
-            this.resetPaint(colorString, color)
-        }
-    }
-
-    /**
-     * 重置画笔
-     */
-    @JvmOverloads
-    fun Paint.resetPaint(colorString: String? = null, @ColorInt color: Int? = null) {
-        this.reset()
-        // 默白色（处理系统渲染抗锯齿时人眼观察到像素色）
-        this.color = color ?: Color.parseColor(colorString ?: "#FFFFFF")
-        this.isAntiAlias = true
-        this.style = Paint.Style.FILL
-        this.strokeWidth = 0f
-    }
-
     /**
      * 绘文本时X轴上垂直居中Y坐标
      */
