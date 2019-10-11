5a867178e90e0367900758d8a2314493ad2f53e1
snpmyn <snpmyn@126.com>
2019-10-11 10:03:38 +0800

    kotlin工具类优化

--- utiltwo/src/main/java/com/zsp/utiltwo/density/DensityUtils.kt
+++ utiltwo/src/main/java/com/zsp/utiltwo/density/DensityUtils.kt
@@ -11,9 +11,18 @@ object DensityUtils {
     /**
      * 设备独立像素转像素
      */
-    fun dipToPxByFloat(context: Context, dpValue: Float): Float {
-        if (dpValue == 0.0f) return 0.0f
+    fun dipToPxByFloat(context: Context, dipValue: Float): Int {
+        if (dipValue == 0.0f) return 0
         val scale = context.resources.displayMetrics.density
-        return (dpValue * scale + 0.5f)
+        return (dipValue * scale + 0.5f).toInt()
+    }
+
+    /**
+     * 像素转设备独立像素
+     */
+    fun pxToDipByInt(context: Context, pxValue: Int): Float {
+        if (pxValue == 0) return 0.0f
+        val scale = context.resources.displayMetrics.density
+        return (pxValue / scale + 0.5f)
     }
 }
